9c05e92618c358c3b3eef2bbb37d4449d6ae9907
VankataG <ivg.ganchev@gmail.com>
2026-07-02 22:16:41 +0300

    Create model for the Bars Spots

---
+++ StreetWorkoutMap/Models/WorkoutSpot.cs
@@ -0,0 +1,11 @@
+﻿namespace StreetWorkoutMap.Models
+{
+    public class WorkoutSpot
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
