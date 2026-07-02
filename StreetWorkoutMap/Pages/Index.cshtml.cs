1225dfc8a958b9df38a191d692ee90dbac5357a4
VankataG <ivg.ganchev@gmail.com>
2026-07-02 22:17:29 +0300

    Add 2 custom markers and show them

--- StreetWorkoutMap/Pages/Index.cshtml.cs
+++ StreetWorkoutMap/Pages/Index.cshtml.cs
@@ -1,13 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using StreetWorkoutMap.Models;
+
 namespace StreetWorkoutMap.Pages
 {
     public class IndexModel : PageModel
     {
+
+        public List<WorkoutSpot> Spots { get; set; } = new();
         public void OnGet()
         {
+            Spots =
+            [
+                new()
+                {
+                    Name = "Марно поле",
+                    Latitude = 43.082,
+                    Longitude = 25.629
+                },
 
+                new()
+                {
+                    Name = "Картала",
+                    Latitude = 43.071,
+                    Longitude = 25.640
+                }
+            ];
         }
     }
 }
