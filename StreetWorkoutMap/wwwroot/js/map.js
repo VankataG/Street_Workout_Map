702d21a591c745b8a92fd2bc46061ff9f9b4ebc3
VankataG <ivg.ganchev@gmail.com>
2026-07-05 19:33:37 +0300

    Separate js map logic in new file

---
+++ StreetWorkoutMap/wwwroot/js/map.js
@@ -0,0 +1,27 @@
+﻿
+const map = L.map('map').setView([43.0757, 25.6172], 14);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+
+const mapElement = document.getElementById('map');
+
+console.log(mapElement.dataset.spots)
+spots = JSON.parse(mapElement.dataset.spots);
+
+spots.forEach(spot => {
+    const popup = `
+                        <h3>${spot.Name}</h3>
+
+                        <p>${spot.Description}</p>
+
+                        <strong>⭐ ${spot.Rating}</strong>
+            `;
+
+    L.marker([spot.Latitude, spot.Longitude])
+        .addTo(map)
+        .bindPopup(popup);
+});
