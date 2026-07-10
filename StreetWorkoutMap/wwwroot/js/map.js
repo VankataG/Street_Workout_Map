eff741798fe82c78448e3e9927e4c3f24f30f01f
VankataG <ivg.ganchev@gmail.com>
2026-07-11 00:41:37 +0300

    Refactor into function, cleaner

--- StreetWorkoutMap/wwwroot/js/map.js
+++ StreetWorkoutMap/wwwroot/js/map.js
@@ -1,25 +1,42 @@
-﻿
-const map = L.map('map').setView([43.0757, 25.6172], 14);
+﻿const mapElement = document.getElementById('map');
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+const map = createMap();
+const spots = JSON.parse(mapElement.dataset.spots);
 
 
-const mapElement = document.getElementById('map');
+addMarkers(map, spots);
 
-console.log(mapElement.dataset.spots)
-spots = JSON.parse(mapElement.dataset.spots);
 
-spots.forEach(spot => {
-    const popup = `
+function createMap() {
+    const map = L.map('map').setView([43.0757, 25.6172], 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    return map;
+}
+
+function addMarkers(map, spots) {
+    spots.forEach(spot => {
+        const popup = createPopup(spot);
+
+        L.marker([spot.Latitude, spot.Longitude])
+            .addTo(map)
+            .bindPopup(popup);
+    });
+}
+
+function createPopup(spot) {
+    return
+            `
                      <div class="spot-popup">
                         <h3>${spot.Name}</h3>
 
                         <p>${spot.Description}</p>
 
-                        <p><strong>Рейтинг:</strong>⭐ ${spot.Rating}</strong>
+                        <p><strong>Рейтинг:</strong>⭐ ${spot.Rating}</p>
 
                         <p>
                             ${spot.HasParallelBars ? "✅ Има успоредка" : "❌ Няма успоредка"}
@@ -30,8 +47,4 @@ spots.forEach(spot => {
                         </p>
                     </div>
             `;
-
-    L.marker([spot.Latitude, spot.Longitude])
-        .addTo(map)
-        .bindPopup(popup);
-});
+}
