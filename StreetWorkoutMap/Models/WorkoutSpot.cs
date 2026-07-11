1f2462bd7bdf0c3e34dd60dd4335b7eeea4802ce
VankataG <ivg.ganchev@gmail.com>
2026-07-12 00:38:03 +0300

    Update WorkoutSpot for the future

--- StreetWorkoutMap/Models/WorkoutSpot.cs
+++ StreetWorkoutMap/Models/WorkoutSpot.cs
@@ -2,18 +2,36 @@
 {
     public class WorkoutSpot
     {
+        public Guid Id { get; set; }
+
+        //General
         public string Name { get; set; } = string.Empty;
-
-        public double Latitude { get; set; }
-
-        public double Longitude { get; set; }
-
         public string Description { get; set; } = string.Empty;
 
-        public int Rating { get; set; }
+        //Location
+        public string City { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        //Rating
+        public double Rating { get; set; }
+
+        //Media
+        public List<string> Images { get; set; } = new();
+
+        //Equipment
+        public bool HasPullUpBars { get; set; }
 
         public bool HasParallelBars { get; set; }
 
         public bool HasRings { get; set; }
+
+        public bool HasMonkeyBars { get; set; }
+
+        //Features
+        public bool HasLighting { get; set; }
+
+        public bool IsIndoor { get; set; }
     }
 }
