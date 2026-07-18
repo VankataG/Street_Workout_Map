61f23fe442a058e7b8f811c9790091beafd7e2c6
VankataG <ivg.ganchev@gmail.com>
2026-07-18 16:12:04 +0300

    Prepare SpotDetailsDto and proper Page

---
+++ StreetWorkoutMap/DTOs/WorkoutSpot/SpotDetailsDto.cs
@@ -0,0 +1,44 @@
+﻿using StreetWorkoutMap.Data;
+using StreetWorkoutMap.Models;
+
+namespace StreetWorkoutMap.DTOs.WorkoutSpot
+{
+    public class SpotDetailsDto
+    {
+        public Guid Id { get; set; }
+
+        //General
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+        //Location
+        public string City { get; set; } = null!;
+        public string? District { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        //Media
+        public List<string> ImageUrls { get; set; } = [];
+
+        //Equipment
+        public bool HasPullUpBars { get; set; }
+
+        public bool HasParallelBars { get; set; }
+
+        public bool HasRings { get; set; }
+
+        //Features
+        public bool HasLighting { get; set; }
+
+        public bool IsIndoor { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        //Other
+        public string? SubmittedByUserId { get; set; }
+
+        public string? SubmittedByUser { get; set; }
+
+        public bool canEdit { get; set; }
+    }
+}
