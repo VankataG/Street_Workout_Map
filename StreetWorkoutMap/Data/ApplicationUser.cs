72e062687045010a82bde1c14368f6837efa7882
VankataG <ivg.ganchev@gmail.com>
2026-07-20 13:45:44 +0300

    Create and apply migration for first and last name in DB

--- StreetWorkoutMap/Data/ApplicationUser.cs
+++ StreetWorkoutMap/Data/ApplicationUser.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace StreetWorkoutMap.Data
 {
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+
+        [PersonalData]
+        [MaxLength(50)]
+        public string? LastName { get; set; }
     }
 }
---
+++ StreetWorkoutMap/Migrations/20260720104405_AddUserFirstAndLastName.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreetWorkoutMap.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFirstAndLastName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
