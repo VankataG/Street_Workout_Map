63072fb73288d9dece3cfbb94741c85358cd109a
VankataG <ivg.ganchev@gmail.com>
2026-07-30 17:56:41 +0300

    add admin pending spots navigation

---
+++ StreetWorkoutMap/Pages/Admin/PendingSpots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StreetWorkoutMap.Pages.Admin
+{
+    public class PendingSpotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
