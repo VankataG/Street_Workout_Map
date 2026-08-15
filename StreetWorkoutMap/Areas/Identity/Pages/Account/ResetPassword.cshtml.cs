a415e811f78efc4ffcecf658c6758cb5ec8cb0f0
VankataG <ivg.ganchev@gmail.com>
2026-08-15 08:27:52 +0300

    Polish Forgot Password feautures with email as well

--- StreetWorkoutMap/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ StreetWorkoutMap/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -41,16 +41,16 @@ namespace StreetWorkoutMap.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage = "Моля въведете имейл адрес.")]
+            [EmailAddress(ErrorMessage = "Моля въведете валиден имейл адрес.")]
             public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Моля въведете нова парола.")]
+            [StringLength(100, ErrorMessage = "Паролата трябва да бъде между {2} и {1} символа.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
@@ -58,9 +58,9 @@ namespace StreetWorkoutMap.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            [Required(ErrorMessage = "Моля потвърдете новата парола.")]
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Паролите не съвпадат.")]
             public string ConfirmPassword { get; set; }
 
             /// <summary>
